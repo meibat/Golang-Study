297b81c859b461793d3057a57115d313c177158f
melissa-mfs <melissa.mf29@gmail.com>
2023-04-10 15:47:59 -0300

    erro no monitoramento do site

--- AcessoSite.go
+++ AcessoSite.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"os"
 	"time"
+	"io"
+	"bufio"
 )
 
 const delay = 3
@@ -16,11 +18,17 @@ func main() {
 		switch opcao {
 		case 1:
 			var numero int
+			var site int
+			sites := listarSites("sites.txt")
+
+			fmt.Println("\nQual site deseja testar?")
+			fmt.Scan(&site)
 			fmt.Println("Quantas vezes deseja testar?")
 			fmt.Scan(&numero)
-			monitoramento(numero)
+
+			monitoramento(numero, site, sites)
 		case 2:
-			print("logs")
+			println("logs")
 		case 0:
 			fmt.Println("Encerrando o programa...")
 			os.Exit(0)
@@ -43,26 +51,60 @@ func introducao() int {
 	return opcao
 }
 
-func monitoramento(numero int) {
-	sites := []string{"https://www.alura.com.br", "https://random-status-code.herokuapp.com/"} //slice
+func listarSites(nomeArquivo string) []string{
+	var sites []string
+	arquivo, err := os.Open(nomeArquivo) 
+	//arquivo, err := ioutil.ReadFile(nomeArquivo) retorna em byte
 
-	for vezes := 0; vezes < numero; vezes++ {
-		for i := 0; i < len(sites); i++ {
-			acessandoSite(sites[i])
-			fmt.Println("-----------------------------")
+	if err != nil{
+		fmt.Println("Ocorreu um erro: ", err)
+		var nulo []string
+		return nulo
+	}
+
+	leitor := bufio.NewReader(arquivo)
+	i := 1
+	for {
+		linha, err := leitor.ReadString('\n')
+		fmt.Print(i,"-", linha)
+		sites = append(sites, linha)
+
+		if err == io.EOF{
+			break
 		}
-		time.Sleep(delay * time.Second)
+		i += 1
+	}
+
+	return sites
+}
+
+func monitoramento(numero int, site int, sites []string) {
+	if site != 0 {
+		for vezes := 0; vezes < numero; vezes++ {
+			acessandoSite(sites[site])
+			fmt.Println("-----------------------------")
+			time.Sleep(delay * time.Second)
+		}
+	} else{
+		for vezes := 0; vezes < numero; vezes++ {
+			for i := 0; i < len(sites); i++ {
+				acessandoSite(sites[i])
+				fmt.Println("-----------------------------")
+			}
+			time.Sleep(delay * time.Second)
+		}
 	}
 }
 
 func acessandoSite(site string) {
 	fmt.Println("Testando site: ", site)
-	resp, _ := http.Get(site)
+
+	resp, err := http.Get(site)
 	fmt.Println("Site Status:", resp.StatusCode)
 
-	if resp.StatusCode == 200 {
-		fmt.Println("O site foi acessado com sucesso!")
+	if err != nil || resp.StatusCode != 200 {
+		fmt.Println("Site com problema! Erro:", err)
 	} else {
-		fmt.Println("Site com problema!")
-	}
+		fmt.Println("O site foi acessado com sucesso!")
+	} 
 }
